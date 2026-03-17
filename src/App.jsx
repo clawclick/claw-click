6e2897baa9d90451911eac1a7cc5b308f61088d6
ClawsFun <clawsfun@proton.me>
2026-03-17 19:39:30 +0000

    Update homepage with new design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomePage from './components/HomePage'
@@ -7,18 +7,27 @@ import ApiDocs from './pages/ApiDocs'
 import AppMarketplace from './pages/AppMarketplace'
 import './pages.css'
 
+function AppContent() {
+  const location = useLocation()
+  const shouldShowFooter = location.pathname !== '/api'
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/api" element={<ApiDocs />} />
+        <Route path="/app" element={<AppMarketplace />} />
+      </Routes>
+      {shouldShowFooter && <Footer />}
+    </div>
+  )
+}
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/api" element={<ApiDocs />} />
-          <Route path="/app" element={<AppMarketplace />} />
-        </Routes>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   )
 }
